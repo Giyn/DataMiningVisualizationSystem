e28b0f8c5512c9223de75cb1501b8164a623b6e8
Uzukidd <uzukidd@gmail.com>
2020-07-26 22:08:41 +0800

    update some internal functions

--- App/utils.py
+++ App/utils.py
@@ -1,9 +1,11 @@
 import numpy as np
-from pandas import DataFrame
+from pandas import DataFrame, get_dummies
 import matplotlib.pyplot as plt
-
-
 
 def dropColumns(dataSet:DataFrame, columns:list)  -> DataFrame:
 
-    dataSet.drop(labels = columns)
+    return dataSet.drop(labels = columns, axis = 1)
+
+def getDummies(dataSet:DataFrame, columns:list)  -> DataFrame:
+
+    return get_dummies(dataSet, columns = columns)
