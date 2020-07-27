7e991f67d6259221dddf8927823ec802fcadc641
Uzukidd <uzukidd@gmail.com>
2020-07-27 17:50:48 +0800

    update the Server API

--- App/main.py
+++ App/main.py
@@ -1,5 +1,9 @@
+import json
+from io import StringIO
+
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
+from pandas import *
 
 app = Flask(__name__)
 
@@ -8,6 +12,42 @@ CORS(app)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/api-DataFrame', methods=['POST', 'GET'])
+def dataFrame() :
+
+    if request.method == 'POST':
+
+        data = json.loads(str(request.data, 'utf-8'))
+
+        text = StringIO(data['dataSet'])
+
+        sep = ','
+
+        if('sep' in data) : sep = data['sep']
+
+        df = read_csv(text, sep = sep)
+
+        res = [['id']]
+
+        for i in range(0, len(df)) :
+
+            res.append([i])
+
+        for i in df.columns :
+
+            res[0].append(i)
+
+            for j in range(1, len(df) + 1):
+
+                res[j].append(str(df[i][j - 1]))
+
+        print(df.columns)
+
+        return str(res)
+
+    return ""
+
 
 @app.route('/api-submit', methods=['POST', 'GET'])
 def submit() :
@@ -28,19 +68,17 @@ def submit() :
 
         form = request.form.to_dict()
 
-        if('dataSet' not in form) : return "feature \"dataSet\" not found"
-
-        dataSet = request.form['dataSet']
+        if('dataSet' in form) : dataSet = request.form['dataSet']
 
         if('dropColumns' in form) : dropColumns = request.form['dropColumns']
 
         if('discreteColumns' in form) : discreteColumns = request.form['discreteColumns']
 
-        print(dataSet)
+        print("dataSet:\n" + str(dataSet))
 
-        print(dropColumns)
+        print("dropColumns:\n" + str(dropColumns))
 
-        print(discreteColumns)
+        print("discreteColumns:\n" + str(discreteColumns))
 
         return "Submit successfully!"
 
@@ -55,4 +93,4 @@ def before_request():
     print("地址[" + str(ip) + "] 试图访问：" + str(url))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug = True)
