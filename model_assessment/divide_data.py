d51ceb51fb0ed9ab7c0c8e451c783ddf469280ff
gjjshgu <56583840+gjjshgu@users.noreply.github.com>
2020-07-29 17:36:04 +0800

    add model-assessment included clf and reg

---
+++ model_assessment/divide_data.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+def train_test_split(X, y, test_ratio, seed=None): # 留出法
+    """
+    :param X: 特征集
+    :param y: 结果集
+    :param test_ratio: 测试集占比
+    :param seed: 随机种子
+    :return:
+    """
+    if seed: np.random.seed(seed)
+    a = len(X)
+    shuffled_indexes = np.random.permutation(a) #打乱索引
+    shuffled_train = shuffled_indexes[:int(a*(1-test_ratio))] #分配索引
+    shuffled_test = shuffled_indexes[int(a*(1-test_ratio)):a]
+    # 根据索引找元素
+    X_train = np.array([X[i] for i in shuffled_train])
+    X_test = np.array([X[i] for i in shuffled_test])
+    y_train = np.array([y[i] for i in shuffled_train])
+    y_test = np.array([y[i] for i in shuffled_test])
+    return X_train, X_test, y_train, y_test
+
+def cross_validation(algorihm, X, y, n): #交叉验证法
+    """
+    :param algorihm: 传入算法的类
+    :param X: 特征集
+    :param y: 结果集
+    :param n: 划分个数
+    :return:
+    """
+    def accuracy_score(y_true, y_predict): # 计算准确率
+        return np.sum(y_true == y_predict) / len(y_true)
+    all = np.random.permutation(len(X)) #打乱索引
+    size = len(X) // n
+    # 根据n把数据分成n部分
+    s = []
+    for i in range(0, len(X), size):
+        a = all[i:i+size]
+        s.append(a)
+    result = np.empty((n,)) # 记录准确率
+    # 遍历s，交叉验证过程
+    for i in range(len(s)):
+        b = s.copy()
+        verify = b.pop(i) # 弹出一个元素做验证集
+        train = np.hstack(b) # 其他元素合并做训练集
+        # 根据索引找元素
+        verify_real_X = np.array([X[i] for i in verify])
+        verify_real_y = np.array([y[i] for i in verify])
+        train_real_X = np.array([X[i] for i in train])
+        train_real_y = np.array([y[i] for i in train])
+        # 代入验证类
+        algorihm.fit(train_real_X, train_real_y)
+        verify_predict = algorihm.predict(verify_real_X) #计算预测值
+        result[i] = accuracy_score(verify_real_y, verify_predict) # 计算准确率
+    return np.mean(result) # 准确率求均值返回
+
+def bootstraping(X, y): #自助法
+    """
+    :param X: 特征集
+    :param y: 结果集
+    :return:
+    """
+    a = len(X)
+    test_index = np.empty((a,), dtype=int) # 索引容器
+    for i in range(a): # 根据数据集长度随机抽取a次
+        test_index[i] = np.random.randint(a)
+    # 根据索引找元素
+    X_train = np.array([X[i] for i in test_index])
+    y_train = np.array([y[i] for i in test_index])
+    X_test = []
+    y_test = []
+    # 没抽到的做测试集
+    for i in range(a):
+        if i not in test_index:
+            X_test.append(X[i])
+            y_test.append(y[i])
+    return X_train, np.array(X_test), y_train, np.array(y_test)
---
+++ model_assessment/reg.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+class reg_assessment:
+    def __init__(self, y_predict, y_test):
+        """
+        回归评估
+        :param y_predict: 预测值
+        :param y_test: 真实值
+        """
+        self.y_predict = y_predict
+        self.y_test = y_test
+        self.MAE = None
+        self.MSE = None
+        self.R_square = None
+        self.RMSE = None
+        self.MAPE = None
+
+    def get_MSE(self):
+        if self.MSE is not None: return self.MSE
+        self.MSE = np.sum((self.y_predict - self.y_test)**2) / len(self.y_predict)
+        return self.MSE
+
+    def get_MAE(self):
+        if self.MAE is not None: return self.MAE
+        self.MAE = np.sum(np.abs(self.y_predict - self.y_test)) / len(self.y_predict)
+        return self.MAE
+
+    def get_RMSE(self):
+        if self.RMSE is not None: return self.RMSE
+        if self.MSE is None: self.get_MSE()
+        self.RMSE = np.sqrt(self.MSE)
+        return self.RMSE
+
+    def get_R2(self):
+        if self.R_square is not None: return self.R_square
+        if self.MSE is None: self.get_MSE()
+        self.R_square = 1 - (self.MSE / np.sum((self.y_test - np.mean(self.y_test))**2))
+        return self.R_square
+
+    def get_MAPE(self):
+        if self.MAPE is not None: return self.MAPE
+        self.MAPE = np.sum(np.abs((self.y_predict - self.y_test) / self.y_test)) / len(self.y_predict)
+        return self.MAPE
+
+    def assessment(self):
+        print("均方误差（MSE）：", self.get_MSE())
+        print("平均绝对误差（MAE）：", self.get_MAE())
+        print("均方根误差（RMSE）：", self.get_RMSE())
+        print("平均绝对百分比误差（MAPE）：", self.get_MAPE())
+        print("R平方：", self.get_R2())
